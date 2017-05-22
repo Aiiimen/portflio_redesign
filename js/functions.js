e02b33059a05fcd0b37a32eeb04803597ec881a1
AimenBenAissa <aimenbenfika@gmail.com>
2017-05-22 17:57:42 +0100

    work section
    work section completed with hover animation

--- js/functions.js
+++ js/functions.js
@@ -3,9 +3,24 @@ $(document).ready(function(){
 //open and close menu
     $(".menu_icon").click(function(){
         $(".menu").show("fast");
-    });
+    })
 
     $(".main_page_logo").click(function(){
       $(".menu").hide("fast");
+    })
+
+    //mouse enter project_container
+    //
+    $(".project_container", this).mouseenter(function(){
+
+      $(".work_description", this).show("fast");
+      $(".type_of_work", this).show("fast");
+      $(".work_done", this).show("fast");
+    })
+
+    $(".project_container", this).mouseleave(function(){
+      $(".work_description", this).hide("fast");
+      $(".type_of_work", this).hide("fast");
+      $(".work_done", this).hide("fast");
     })
 });
